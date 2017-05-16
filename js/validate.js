a22a32bd553a1a9069931020314000cc1e31e32b
dollysingh3192 <dollysingh3192@gmail.com>
2017-05-16 17:36:14 +0530

    Form submitting stated

--- js/validate.js
+++ js/validate.js
@@ -46,3 +46,21 @@ function textCounter(membershortdescription, counterID, minLen) {
     }
 
 }
+
+function validateForm()
+{
+    var f = document.forms["theform"].elements;
+    var cansubmit = true;
+
+    for (var i = 0; i < f.length; i++) {
+        if (f[i].value.length == 0) 
+            cansubmit = false;
+    }
+
+    if (cansubmit) {
+        document.getElementById('submitbutton').disabled = false;
+    }
+    else {
+        document.getElementById('submitbutton').disabled = 'disabled';
+    }
+}
